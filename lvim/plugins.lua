725b2ea1ddbc11e42a4cd33c9d8da6464df116a7
FxmousKid <iyan005@outlook.com>
2024-03-18 00:52:26 +0100

    Removing chatgpt plugin from lazy.nvim; no need for it

--- lvim/plugins.lua
+++ lvim/plugins.lua
@@ -84,19 +84,6 @@ local core_plugins = {
     end
   },
   {
-  "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim",
-      "nvim-telescope/telescope.nvim"
-    }
-  },
-  {
     "mg979/vim-visual-multi",
   },
   {
