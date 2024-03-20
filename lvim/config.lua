eed142b5746abba397bc2a3ebbad9b5d465d4733
FxmousKid <iyan005@outlook.com>
2024-03-20 20:10:11 +0100

    Added keybind for ":nog" Command in config.lua

--- lvim/config.lua
+++ lvim/config.lua
@@ -42,7 +42,7 @@ vim.opt.guicursor = {
 -- Keymaps
 local keymap_opts = { noremap = true, silent = true }
 vim.keymap.set({'n','i'}, '<leader>l', function() vim.cmd.DocsViewToggle() end, keymap_opts)
-
+vim.keymap.set({'n','i'}, '<leader>p', function() vim.cmd.noh() end, keymap_opts)
 
 
 
