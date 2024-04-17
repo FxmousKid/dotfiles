d44cf4b2ea4071809df49f013dba7602825104cc
FxmousKid <iyan005@outlook.com>
2024-04-17 17:40:46 +0200

    updating config.lua

--- lvim/config.lua
+++ lvim/config.lua
@@ -41,5 +41,3 @@ local keymap_opts = { noremap = true, silent = true }
 vim.keymap.set({'n','i'}, '<leader>l', function() vim.cmd.DocsViewToggle() end, keymap_opts)
 vim.keymap.set({'n','i'}, '<leader>p', function() vim.cmd.noh() end, keymap_opts)
 
-
-
