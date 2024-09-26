eb1762f6f099e26f1677ba2a55c3b8bcf0103800
FxmousKid <iyan005@outlook.com>
2024-09-26 14:37:59 +0200

    Modified keybinds to add documentation on dashboard

--- lvim/lua/keybinds.lua
+++ lvim/lua/keybinds.lua
@@ -1,5 +1,6 @@
 -- Nvim navigation keymaps
 
 local keymap_opts = { noremap = true, silent = true }
-vim.keymap.set({'n','i'}, '<leader>l', function() vim.cmd.DocsViewToggle() end, keymap_opts)
-vim.keymap.set({'n','i'}, '<leader>p', function() vim.cmd.noh() end, keymap_opts)
+vim.keymap.set({'n','i'}, '<leader>l', function() vim.cmd.DocsViewToggle() end, {table.unpack(keymap_opts), desc="Show Documentation"})
+vim.keymap.set({'n','i'}, '<leader>p', function() vim.cmd.noh() end, {table.unpack(keymap_opts), desc="Clear Search Highlight"})
+vim.keymap.set({'n','i'}, '<leader>-', function() vim.cmd.MarkdownPreviewToggle() end, {table.unpack(keymap_opts), desc="Toggle Markdown Preview"})
