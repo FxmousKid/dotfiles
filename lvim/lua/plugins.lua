d462fe1b381261a60adcdb3af773d38b2480a075
Iyan Nazarian <36068362+FxmousKid@users.noreply.github.com>
2025-07-24 19:18:45 +0300

    fix(lvim): removing buggy line

--- lvim/lua/plugins.lua
+++ lvim/lua/plugins.lua
@@ -192,6 +192,13 @@ lvim.plugins = {
 		end,
 	},
 
+	{
+		 "OXY2DEV/markview.nvim",
+		lazy = false,
+		-- For `nvim-treesitter` users.
+		priority = 49,
+	}
+
 
 	--{
 	-- 	"neovim/nvim-lspconfig",
