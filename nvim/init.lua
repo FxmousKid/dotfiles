9269933cdfa4e75973fbf46de6e8c8aa3b0c3b11
Iyan Nazarian <36068362+FxmousKid@users.noreply.github.com>
2025-07-30 23:37:01 +0300

    feat(nvim): starting nvim confi from scratchg

---
+++ nvim/init.lua
@@ -0,0 +1,19 @@
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.number = true
+vim.opt.autoindent = true
+
+vim.opt.whichwrap:append {
+	['<'] = true,
+  	['>'] = true,
+  	['['] = true,
+  	[']'] = true,
+  	h = true,
+  	l = true,
+}
+
+vim.g.mapleader = " "
+
+-- Other config files
