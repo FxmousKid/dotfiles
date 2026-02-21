feecdfa85d7986b08a1ca6b2a8fe71abba47ebcc
Iyan Nazarian <36068362+FxmousKid@users.noreply.github.com>
2026-02-21 16:13:36 +0100

    chore(lvim): removing useless  plugins

--- lvim/lua/plugins.lua
+++ lvim/lua/plugins.lua
@@ -7,7 +7,6 @@ lvim.plugins = {
 	{ "mg979/vim-visual-multi" },
 	{ "mfussenegger/nvim-jdtls" },
 	{ "j-hui/fidget.nvim" }, -- Better Notifications
-
 	{
 		"danymat/neogen",
 		config = true,
@@ -16,7 +15,6 @@ lvim.plugins = {
 	},
 
 	{
-
 		"FabijanZulj/blame.nvim",
 		lazy = false,
 		config = function()
@@ -25,7 +23,6 @@ lvim.plugins = {
 		opts = {
 			blame_options = { '-w' },
 		},
-
 	},
 
 	-- Better Navigation
@@ -64,30 +61,6 @@ lvim.plugins = {
 	{
 		"meznaric/key-analyzer.nvim",
 		opts = {}
-	},
-
-    {
-        'brianhuster/live-preview.nvim',
-        dependencies = {
-            --'brianhuster/autosave.nvim',  -- Not required, but recomended for autosaving and sync scrolling
-            'nvim-telescope/telescope.nvim' -- Not required, but recommended for integrating with Telescope
-        },
-        opts = {
-
-
-			commands = {
-				start = 'LivePreview', -- Command to start the live preview server and open the default browser.
-				stop = 'StopPreview', -- Command to stop the live preview. 
-			},
-			port = 5500, -- Port to run the live preview server on.
-			autokill = false, -- If true, the plugin will autokill other processes running on the same port (except for Neovim) when starting the server.
-			browser = 'default', -- Terminal command to open the browser for live-previewing (eg. 'firefox', 'flatpak run com.vivaldi.Vivaldi'). By default, it will use the default browser.
-			dynamic_root = false, -- If true, the plugin will set the root directory to the previewed file's directory. If false, the root directory will be the current working directory (`:lua print(vim.uv.cwd())`).
-			sync_scroll = false, -- If true, the plugin will sync the scrolling in the browser as you scroll in the Markdown files in Neovim.
-			telescope = {
-				true, -- If true, the plugin will automatically load the `Telescope livepreview` extension.
-			},
-		},
 	},
 
 	{
