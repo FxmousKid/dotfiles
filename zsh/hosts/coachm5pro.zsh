21f64daa2b0516114ef077860260573d48d8a0f1
Iyan Nazarian <36068362+FxmousKid@users.noreply.github.com>
2026-07-20 15:28:25 +0200

    feat(zsh): m5 zsh config

--- zsh/hosts/coachm5pro.zsh
+++ zsh/hosts/coachm5pro.zsh
@@ -1,1 +1,21 @@
+# bob (nvim manager)
 export PATH="$HOME/.local/share/bob/nvim-bin:$PATH"
+
+# coast
+export PATH="$HOME/.coast/bin:$PATH"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+# bun completions
+[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
+
+
+# android
+export PATH="$PATH:$HOME/Library/Android/sdk/platform-tools"
+
+
+# sdkman (java)
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
